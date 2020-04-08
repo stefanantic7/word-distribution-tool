13640d1cd372a9961ca5351f117ccb6f49241261
antic <santic16@raf.rs>
2020-04-08 20:20:19 +0200

    Show error if the user enters invalid input for arity

--- src/main/java/rs/raf/word_distribution/client/actions/cruncher/AddCruncherAction.java
+++ src/main/java/rs/raf/word_distribution/client/actions/cruncher/AddCruncherAction.java
@@ -2,6 +2,7 @@ package rs.raf.word_distribution.client.actions.cruncher;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextInputDialog;
 import rs.raf.word_distribution.AppCore;
 import rs.raf.word_distribution.Output;
@@ -31,7 +32,17 @@ public class AddCruncherAction implements EventHandler<ActionEvent> {
 
         Optional<String> result = dialog.showAndWait();
         result.ifPresent(arityString -> {
-            int arity = Integer.parseInt(result.get());
+            int arity = 0;
+            try {
+                arity = Integer.parseInt(result.get());
+            } catch (NumberFormatException numberFormatException) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setHeaderText("Your input should be a number.");
+                alert.setContentText("Please create another one.");
+                alert.showAndWait();
+                return;
+            }
 
             CounterCruncher counterCruncher = new CounterCruncher(arity, AppCore.getCruncherThreadPool());
             counterCruncher.addOutput(this.output);
--- src/main/java/rs/raf/word_distribution/client/actions/cruncher/RemoveCruncherAction.java
+++ src/main/java/rs/raf/word_distribution/client/actions/cruncher/RemoveCruncherAction.java
@@ -3,6 +3,7 @@ package rs.raf.word_distribution.client.actions.cruncher;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.layout.Pane;
+import rs.raf.word_distribution.client.views.MainStage;
 import rs.raf.word_distribution.counter_cruncher.CounterCruncher;
 import rs.raf.word_distribution.client.views.cruncher.CruncherDetailsBox;
 
@@ -18,6 +19,12 @@ public class RemoveCruncherAction implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent event) {
+        // Remove from all inputs
+        MainStage.getInstance().getFileInputsView().getFileInputsSet().forEach(fileInput -> {
+            fileInput.unlinkCruncher(counterCruncher);
+        });
+        // TODO remove from list view
+
         counterCruncher.destroy();
         ((Pane) this.cruncherDetailsBox.getParent()).getChildren().remove(this.cruncherDetailsBox);
         this.cruncherDetailsBox.getCruncherObservableList().remove(counterCruncher);
